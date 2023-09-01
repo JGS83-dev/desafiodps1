456fcab770b6e75b6690850335416654cdd623e1
WilfredoAcosta <wilfredo.acosta10@gmail.com>
2023-09-01 08:38:35 -0600

    Base de formulario

--- src/App.js
+++ src/App.js
@@ -1,16 +1,127 @@
-import React from 'react';
-import { Button } from 'antd';
-import './App.css';
-
-function App() {
+import { PlusOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import {
+  Button,
+  Cascader,
+  Checkbox,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+  Select,
+  Slider,
+  Switch,
+  TreeSelect,
+  Upload,
+} from 'antd';
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
+const normFile = (e) => {
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e?.fileList;
+};
+const FormDisabledDemo = () => {
+  const [componentDisabled, setComponentDisabled] = useState(false);
   return (
-    <div className="App">
-      <Button type="primary">Primary</Button>
-      <Button type="default">Default</Button>
-      <Button type="dashed">Dashed</Button>
-      <Button type="link">Link</Button>
-    </div>
+    <>
+     
+      <Form
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 14,
+        }}
+        layout="horizontal"
+        style={{
+          maxWidth: 600,
+        }}
+      >
+      
+        <Form.Item label="Nombre">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Apellido">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Idioma">
+          <Select>
+            <Select.Option value="demo">Ingles</Select.Option>
+            <Select.Option value="demo">Frances</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item label="TreeSelect">
+          <TreeSelect
+            treeData={[
+              {
+                title: 'Light',
+                value: 'light',
+                children: [
+                  {
+                    title: 'Bamboo',
+                    value: 'bamboo',
+                  },
+                ],
+              },
+            ]}
+          />
+        </Form.Item>
+        <Form.Item label="Cascader">
+          <Cascader
+            options={[
+              {
+                value: 'zhejiang',
+                label: 'Zhejiang',
+                children: [
+                  {
+                    value: 'hangzhou',
+                    label: 'Hangzhou',
+                  },
+                ],
+              },
+            ]}
+          />
+        </Form.Item>
+        <Form.Item label="DatePicker">
+          <DatePicker />
+        </Form.Item>
+        <Form.Item label="RangePicker">
+          <RangePicker />
+        </Form.Item>
+        <Form.Item label="InputNumber">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item label="TextArea">
+          <TextArea rows={4} />
+        </Form.Item>
+        <Form.Item label="Switch" valuePropName="checked">
+          <Switch />
+        </Form.Item>
+        <Form.Item label="Upload" valuePropName="fileList" getValueFromEvent={normFile}>
+          <Upload action="/upload.do" listType="picture-card">
+            <div>
+              <PlusOutlined />
+              <div
+                style={{
+                  marginTop: 8,
+                }}
+              >
+                Upload
+              </div>
+            </div>
+          </Upload>
+        </Form.Item>
+        <Form.Item label="Button">
+          <Button>Button</Button>
+        </Form.Item>
+        <Form.Item label="Slider">
+          <Slider />
+        </Form.Item>
+      </Form>
+    </>
   );
-}
-
-export default App;
+};
+export default () => <FormDisabledDemo />;
