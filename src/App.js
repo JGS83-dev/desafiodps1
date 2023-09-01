ed47855f3d007599871eeb9922433ae57e0fa228
jgs23 <js8215001@gmail.com>
2023-09-01 08:06:44 -0600

    calculo inicial de precio a pagar

--- src/App.js
+++ src/App.js
@@ -9,16 +9,24 @@ const MainComponent = () => {
   const [nombre, setNombre] = useState("");
   const [apellido, setApellido] = useState("");
   const [cursoSeleccionado, setCursoSeleccionado] = useState("");
-  const [precioFinal, setPrecioFinal] = useState("");
+  const [precioFinal, setPrecioFinal] = useState(0);
+  const [descuentoAplicado, setDescuentoAplicado] = useState(0);
 
   const selectIdiomasDisponibles = ["ingles", "frances"];
 
-  const CalcularDescuento = () => {
-    let meses = 1;
+  const CalcularDescuento = (meses) => {
     descuentos.forEach((e) => {
-      if (e.inferior <= meses && e.superior >= meses) {
-        setPrecioFinal(25 + 25 * (e.descuento / 100));
-        console.log(precioFinal);
+      console.log('Limite superior',e.inferior);
+      console.log('Limite inferior',e.inferior);
+      if (meses >= e.inferior && meses <= e.superior) {
+        if(e.descuento > 0){
+          setDescuentoAplicado(e.descuento)
+          setPrecioFinal(25 + 25 * (e.descuento / 100));
+        }else{
+          setDescuentoAplicado(0)
+          setPrecioFinal(25);
+        }
+        console.log('Precio con descuento',precioFinal);
       }
     });
   };
@@ -45,8 +53,9 @@ const MainComponent = () => {
         </Form.Item>
         <Form.Item label="Idioma">
           <Select>
-            <Select.Option value="demo">Ingles</Select.Option>
-            <Select.Option value="demo">Frances</Select.Option>
+            {selectIdiomasDisponibles.forEach((element) => {
+              <Select.Option value={element}>{element}</Select.Option>;
+            })}
           </Select>
         </Form.Item>
 
@@ -55,7 +64,7 @@ const MainComponent = () => {
         </Form.Item>
 
         <Form.Item>
-          <Button>Submit</Button>
+          <Button onClick={() => CalcularDescuento(2)}>Calcular</Button>
         </Form.Item>
         <Form.Item>
           <Input placeholder="Descuento" disabled />
